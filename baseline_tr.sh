9eeb4d9c8c3f6c31e2520280386822a6637398eb
Pietro <pitomihelj@gmail.com>
2025-08-06 10:18:15 +0200

    stessa migliorira organizzazione slurm per baseline

--- baseline_tr.sh
+++ baseline_tr.sh
@@ -6,8 +6,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=128GB
 #SBATCH --time=2-00:00:00
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
+#SBATCH --output=slurms/baselines/secondo_training/slurm-%j.out
+#SBATCH --error=slurms/baselines/secondo_training/slurm-%j.err
 source /opt/spack/opt/spack/linux-rocky9-x86_64/gcc-13.2.0/miniconda3-22.11.1-tn534fvb4uy4wrf7m2zcwpiycdzlebd6/bin/activate my_tesy_env
 cd /u/pmihelj/TESI-TRIENNALE
 python3 -u baseline_models_training.py \
