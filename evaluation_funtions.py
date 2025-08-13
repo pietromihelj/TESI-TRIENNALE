967d23c9fa527031a48f9e02d667703bb469a8a5
Pietro <pitomihelj@gmail.com>
2025-08-13 16:15:23 +0200

    inizio delle funzioni di evaluation, fase gia fatta da fare la connettività

--- evaluation_funtions.py
+++ evaluation_funtions.py
@@ -3,6 +3,10 @@ import numpy as np
 import pickle
 from model import VAEEG
 from sklearn.decomposition import  PCA, FastICA, KernelPCA
+import pywt 
+import pandas as pd
+
+
 
 def pearson_index(x, y, dim=-1):
         #calcolo manuale del coeff di correlazione di pearson lungo una specifica dimensione
@@ -26,6 +30,7 @@ def pearson_index(x, y, dim=-1):
         r = (mxy - mx * my) / torch.sqrt(varx*vary)
         return r
 
+
 def NRMSE(x,y):
     squared_error = (x - y) ** 2
     mse = np.mean(squared_error)
@@ -35,6 +40,7 @@ def NRMSE(x,y):
         return np.inf
     nrmse = rmse / mean_x
     return nrmse
+
 
 class GetLatentVAEEG():
     
@@ -81,6 +87,38 @@ class GetLatentBaseline():
 
         return zs, x_recon
     
-        
+
+class PhaseComparison():
+    def __init__(self):
+        self.BANDS = [("delta", (1.0, 4.0)),
+                  ("theta", (4.0, 8.0)),
+                  ("alpha", (8.0, 13.0)),
+                  ("low_beta", (13, 20)),
+                  ("high_beta", (20, 30.0))]
     
-    
+    def compare_morlet_wavelet_phase(self, orig, rec, l_freq, h_freq, fs=250):
+        #creo la lista di scale
+        scales=np.arange(fs/2/h_freq, fs/2/l_freq, 1)
+        #alcolo le trasformate wavelet morlet
+        coeff_orig, freq = pywt.cwt(orig, wavelet='cmor', scales=scales, sampling_period=1/fs)
+        coeff_rec, freq = pywt.cwt(rec, wavelet='cmor', scales=scales, sampling_period=1/fs)
+        #calcolo la fase dei segnali
+        orig_phase, rec_phase = np.angle(coeff_orig), np.angle(coeff_rec)
+
+        res = {}
+        for k,v in self.BANDS.items():
+            pick_index = np.logical_and(freq<=v[1], freq>v[0])
+            mean_mae = np.abs((orig_phase[pick_index] - rec_phase[pick_index])).mean()
+            res[k] = [mean_mae]
+        return pd.DataFrame(res)
+
+    def work(self, model, save_path, orig, rec):
+        res = []
+        for o,r in zip(orig, rec):
+            tmp_res = self.compare_morlet_wavelet_phase(orig=o, rec=r, l_freq=1, h_freq=30, fs=250)
+            res.append(tmp_res)
+        res = pd.concat(res, axis=0)
+        res.to_csv(save_path, index=False)
+
+
+class ConComparison():
