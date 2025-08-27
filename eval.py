f05613ad8d5e89627f5b07821e4d373b358c3e2d
pietromihelj <pitomihelj@gmail.com>
2025-08-27 15:22:23 +0200

    new device

--- eval.py
+++ eval.py
@@ -60,7 +60,7 @@ print('pc_mae=',pc_maes)
 print('pearson_mae=', pearson_maes)
 print('nrmse_mae=', nrmse_maes)
 
-values = [np.array([pcc_con_maes]), pvl_maes[0], np.array([pc_maes])]
+values = [np.array([pcc_con_maes]), pvl_maes[0], [pc_maes][0]]
 values_names = ['Correlation Mean Absolute Error', 'PVL Mean Absolute Error', 'Phase Mean Absolute Error']
 y_labs = ['Correlation MAE', 'PVL MAE', 'Phase MAE[rad]']
 
